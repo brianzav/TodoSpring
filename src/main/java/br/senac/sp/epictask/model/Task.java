cc195cf15390879e99268cd17e6f546d00823055
brian.bzculque <brianzav23@gmail.com>
2023-11-17 22:35:12 -0200

    Add validation rules

--- src/main/java/br/senac/sp/epictask/model/Task.java
+++ src/main/java/br/senac/sp/epictask/model/Task.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Entity
@@ -11,8 +12,17 @@ import lombok.Data;
 public class Task {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank
     String title;
+
+    // not null
+    //         not empty
+
+    @Size(min = 10)
     String description;
+
+    @Min(1)
     Integer score;
 
 }
