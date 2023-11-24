6a0e31ffb475b7a0feffc39861c67712339edf25
brian.bzculque <brianzav23@gmail.com>
2023-11-24 21:22:30 -0200

    add delete method

--- src/main/java/br/senac/sp/epictask/controller/TaskController.java
+++ src/main/java/br/senac/sp/epictask/controller/TaskController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 @RequestMapping("task")
@@ -45,4 +45,10 @@ public class TaskController {
     }
 
 
-}
+        @DeleteMapping("{id}")
+        public String delete(@PathVariable Long id){
+            repository.deleteById(id);
+            return "redirect:/task";
+        }
+    }
+
